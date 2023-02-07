07c09c2bd2f2c09eac05e83e70143fc966aad3b2
fabioishizu <f.masaoishizu@gmail.com>
2023-02-07 16:54:18 -0300

    Primeiro commit + Post para adicionar um filme.

---
+++ ApiFilme/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using ApiFilme.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFilme.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        
+        filmes.Add(filme);
+        Console.WriteLine($"Duracao: {filme.TempoDuracao} Nome: {filme.Titulo} Genero: {filme.Genero}");
+    }
+}
---
+++ ApiFilme/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFilme.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O Título do filme precisa ser preenchido.")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O Gênero do filme precisa ser preenchido.")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero do filme precisa ser inferior a 50 caracteres.")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "O Tempo de duração do filme precisa ser preenchido.")]
+    [Range(70, 600, ErrorMessage = "A duração precisa ser entre 70 e 600 minutos.")]
+    public int TempoDuracao { get; set; }
+
+    public Filme(string titulo, string genero, int tempoDuracao)
+    {
+        Titulo = titulo;
+        Genero = genero;
+        TempoDuracao = tempoDuracao;
+    }
+}
